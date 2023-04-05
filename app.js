334556c4f52042a0a7fc74a9d0eecc36711cd2c7
p.n.tran2017@gmail.com <p.n.tran2017@gmail.com>
2023-04-05 15:22:01 +0700

    change port

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.x6ooqfl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
   .then(() => {
-    app.listen(process.env.PORT || 10000); //process.env.PORT ||
+    app.listen(5000); //process.env.PORT ||
   })
   .catch((err) => {
     console.log(err);
