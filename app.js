536ee6ca0eca4a3bf1f0492736f60f8c0b9b3b6e
p.n.tran2017@gmail.com <p.n.tran2017@gmail.com>
2023-04-07 00:27:25 +0700

    fix bugs

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://34.126.137.84");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
