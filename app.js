e069763d35ac8340fff95485ca5de6f830613ddb
p.n.tran2017@gmail.com <p.n.tran2017@gmail.com>
2024-05-04 19:12:09 +0700

    update code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("./node_modules/mongoose");
+const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 if (process.env.NODE_ENV !== "production") {
